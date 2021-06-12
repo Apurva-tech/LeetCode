e6cd17d42cd41ca63d44de5185952f923e375124
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2021-06-12 22:42:08 +0530

    Time: 4 ms (89.43%), Space: 38.8 MB (32.94%) - LeetHub

--- evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,38 +1,42 @@
 class Solution {
+    
+    public boolean isOperator (String token) {
+        
+        return token.equals ("/") || token.equals ("*") || token.equals ("+") || token.equals ("-");
+    }
+    
+    public int calculate (int a, int b, String token) {
+        
+        if (token.equals ("/")) {
+            return a / b;
+        }
+        else if (token.equals ("*")) {
+            return a * b;
+        }
+        else if (token.equals ("+")) {
+            return a + b;
+        }
+        else {
+            return a - b;
+        }
+    }
+    
     public int evalRPN(String[] tokens) {
-        Stack<String> stack = new Stack<String>();
-        String operations = "+-*/"; 
-        Integer ans = 0; 
-        for(String t : tokens){
-            
-            if(operations.contains(t)) {
-                
-                int n2 = Integer.valueOf(stack.pop());
-                int n1 = Integer.valueOf(stack.pop()); 
-                
-                switch(t){
-                case "+":
-                    ans = n1+n2; 
-                    break; 
-                case "-":
-                    ans = n1-n2;
-                    break; 
-                case "*":
-                    ans = n1*n2;
-                    break; 
-                case "/":
-                    ans = n1/n2;
-                    break;
-                default: 
-                    break; 
+        
+        Stack<Integer> stack  = new Stack<> ();
+        
+        for (String token : tokens) {
+            if (isOperator (token)) {
+                int b = stack.pop ();
+                int a = stack.pop ();
+                int val = calculate (a, b, token);
+                stack.push (val);
             }
-            stack.push(Integer.toString(ans)); 
+            else {
+                stack.push (Integer.valueOf (token));
             }
-            else{
-            stack.push(t); 
         }
-        }
-        ans = Integer.valueOf(stack.pop());
-        return ans; 
+        
+        return stack.pop ();
     }
 }
