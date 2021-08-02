41f6a20075cca6f27ec545941a21348321c03b3f
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2021-08-03 01:13:38 +0530

    Time: 8 ms (95.50%), Space: 10.7 MB (50.56%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& res, int k) {
+        vector<int> ans;  
+        unordered_map<int, int> mp; 
+        
+        for(int i = 0; i < res.size(); ++i){
+            if(mp.find(k - res[i])!=mp.end()){
+                ans.push_back(i); 
+                ans.push_back(mp[k-res[i]]); 
+                return ans;
+            }
+            else mp[res[i]] = i; 
+        }
+        
+        return ans;  
+    }
+};
