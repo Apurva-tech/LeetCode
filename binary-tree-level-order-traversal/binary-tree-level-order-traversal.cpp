fb9f1d068bbf2e9a72677a1e39aa000dbfdec509
Apurva Sharma <apurva.sharma866@gmail.com>
2021-08-20 22:56:57 +0530

    Commit

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -9,40 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans; 
-        if(!root) return ans; 
-        
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        vector<vector<int>> ans;
+        if (!root)
+            return ans;
+
         // initialise queue
-        queue<TreeNode *> q; 
-        q.push(root); 
-        
-        while(!q.empty()){
-            int currSize = q.size(); 
-            vector<int> currList; 
-            
-            while(currSize > 0){
-            // Dequeue element
-            TreeNode *currNode = new TreeNode();
-            currNode = q.front();
-            
-            currList.push_back(currNode->val);
-            currSize -= 1;
-            q.pop();
- 
-            //Check for left child
-            if (currNode->left)
-                q.push(currNode->left);
-            //Check for right child
-            if (currNode->right)
-                q.push(currNode->right);
-            
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int currSize = q.size();
+            vector<int> currList;
+
+            while (currSize > 0)
+            {
+                // Dequeue element
+                TreeNode *currNode = new TreeNode();
+                currNode = q.front();
+
+                currList.push_back(currNode->val);
+                currSize -= 1;
+                q.pop();
+
+                if (currNode->left)
+                    q.push(currNode->left);
+                //Check for right child
+                if (currNode->right)
+                    q.push(currNode->right);
             }
-            ans.push_back(currList); 
+            ans.push_back(currList);
         }
-        
-        return ans; 
+
+        return ans;
     }
 };
