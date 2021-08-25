6deed007132999142a296713bf7aed362b29c9e5
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2021-08-25 13:39:11 +0530

    Time: 336 ms (15.61%), Space: 67.8 MB (24.33%) - LeetHub

---
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,30 @@
+typedef pair<double, int> pi;
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<double> nums; 
+        for(int i = 0; i < points.size(); i++){ 
+            double t = sqrt(points[i][0]*points[i][0] + points[i][1]*points[i][1]);
+            nums.push_back(t); 
+        }
+        
+        unordered_map<int, double>mp; 
+        
+        for(int i = 0; i < points.size(); i++) mp[i] = nums[i];
+        
+        priority_queue<pi, vector<pi>, greater<pi> > pq;
+        
+        unordered_map<int, double> :: iterator itr;
+        for(itr=mp.begin();itr!=mp.end();itr++){
+            cout << "dist: " << itr->second << " ind: " << itr->first << endl; 
+            pq.push(make_pair(itr->second , itr->first));
+        }
+        vector<vector<int>> ans(k);
+        for(int i=0;i<k;i++){
+            ans[i]=points[pq.top().second];
+            pq.pop();
+        }
+        return ans;
+    }
+};
