2fa04fe9eeab34e10c9bec9fdb4f873df8565abd
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2021-08-26 19:59:30 +0530

    Time: 164 ms (9.13%), Space: 47.3 MB (5.02%) - LeetHub

---
+++ ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    int nthUglyNumber(int n) {
+        set<long long> p, q;
+        p.insert(1); 
+        while((int)q.size() != n){
+            long long next = *p.begin();
+            q.insert(next); 
+            p.erase(next); 
+            p.insert(2*next); 
+            p.insert(3*next); 
+            p.insert(5*next); 
+        }
+        return *q.rbegin(); 
+    }
+};
