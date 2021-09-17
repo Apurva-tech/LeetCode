a2856a84b125712bc3505106dba7911003e84dc2
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2021-09-17 23:54:57 +0530

    Time: 297 ms (8.98%), Space: 10.2 MB (44.01%) - LeetHub

--- count-primes/count-primes.cpp
+++ count-primes/count-primes.cpp
@@ -4,10 +4,12 @@ public:
         if(n == 0 || n == 1) return 0;
         vector<bool>primes(n+1, true);
         primes[0] = primes[1] = false;
+        
         for(int i = 2; i*i <= n; i++)
             if(primes[i])
                 for(int j = 2; i*j <= n; j++)
                     primes[i*j] = false;
+        
         
         int ans = 0;
         for(int i = 2; i < n; i++)
