3b309bd2ba362c162c1f552fd37f89f200162311
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2021-10-29 23:32:19 +0530

    Time: 40 ms (88.94%), Space: 41.3 MB (63.79%) - LeetHub

---
+++ sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    int sumEvenGrandparent(TreeNode* root) {
+       if(!root) return 0;
+    if(root->val%2==0){
+        if(root->left)
+        {if(root->left->left) sum+=root->left->left->val;
+        if(root->left->right) sum+=root->left->right->val;}
+        if(root->right){
+        if(root->right->left) sum+=root->right->left->val;
+        if(root->right->right) sum+=root->right->right->val;
+        }
+    }
+        sumEvenGrandparent(root->left);
+        sumEvenGrandparent(root->right);
+        return sum;
+    }
+};
