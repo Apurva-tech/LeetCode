0146a42f140f900196c1b96e46c45b65f6ee38ad
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2021-12-10 23:50:59 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.76%) - LeetHub

---
+++ maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int n = num, i = 0, j = -1;
+        
+        while (n) {
+            if (n%10 == 6) {
+                j = i;
+            }
+            i++;
+            n/=10;
+        }
+        
+        if (j == -1)
+            return num;
+        
+        return num + pow(10, j)*3;
+    }
+};
