c2cb1f8bb2553852baf8b31f643ab32f8d50deb3
Apurva Sharma <apurva.sharma866@gmail.com>
2021-12-28 19:07:51 +0530

    done

--- partition-list/partition-list.cpp
+++ partition-list/partition-list.cpp
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
