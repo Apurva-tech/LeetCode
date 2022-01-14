235b22b1b5260a735d1e54869ac0c891e2a2cac4
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2022-01-14 20:36:13 +0530

    Time: 140 ms (27.57%), Space: 85.2 MB (5.08%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        if(root == NULL) return 0;
+        int n = 0;
+        queue<TreeNode*> q; 
+        q.push(root);
+        
+        while(!q.empty()){
+            int size = q.size();
+            while(size > 0){
+                TreeNode* curr = new TreeNode();
+                curr = q.front();
+                size --; 
+                q.pop(); 
+                if(curr->left) q.push(curr->left); 
+                if(curr->right) q.push(curr->right); 
+                
+            }
+            n++; 
+        }
+        
+        q.push(root);
+        
+        int rs = 0;
+        
+        while(!q.empty()){
+            vector<int> temp;
+            int size = q.size();
+            while(size > 0){
+                TreeNode* curr = new TreeNode();
+                curr = q.front();
+                size --; 
+                q.pop(); 
+                if(curr->left) q.push(curr->left); 
+                if(curr->right) q.push(curr->right); 
+                temp.push_back(curr->val);
+            }
+            
+            if(n!=0) {
+                    n--;
+                    cout << " " << n << "\n";
+                }
+            if(n==0) {
+                    for(auto x:temp) rs += x;
+                }
+        }
+        
+        
+        return rs; 
+    }
+};
