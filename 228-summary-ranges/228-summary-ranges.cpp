7f07ca228d4f44ceb2eb8a0d7838698ea58001dd
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2022-02-28 16:00:56 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (14.46%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        if(nums.size() == 1) return {to_string(nums[0])}; 
+        vector<string> ans;
+        for(int i = 0; i < nums.size(); i++){
+            int s = nums[i], l = INT_MIN; 
+            if(i < nums.size()-1 && nums[i+1] == (nums[i] + 1)){
+                while(i < nums.size()-1 && nums[i+1] == nums[i] + 1) i++; 
+                l = nums[i];
+            }
+            // cout << i << " ";
+            if(l != INT_MIN) ans.push_back(to_string(s) + "->" + to_string(l) ); 
+            else ans.push_back(to_string(s)); 
+        } 
+        return ans;
+    }
+};
