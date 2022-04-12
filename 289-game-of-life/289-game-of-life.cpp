7e262187a72b995579d4e722ab281c31cf134ce9
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2022-04-12 23:53:51 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (96.30%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int m=board.size(),n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int count=0;
+                for(int I=max(i-1,0);I<min(i+2,m);I++){
+                    for(auto J=max(j-1,0);J<min(j+2,n);J++){
+                        count+=board[I][J]&1; 
+                    }
+                }
+                    if(count==3 || count-board[i][j]==3)
+                        board[i][j]|=2; 
+                }
+            }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] >>=1;  
+            }
+        }
+    }
+};
