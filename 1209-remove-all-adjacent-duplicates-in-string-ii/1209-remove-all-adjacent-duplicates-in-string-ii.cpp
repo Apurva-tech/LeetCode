f416556ddc4d5e617e11fc1b7922354d9e625e2c
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2022-05-06 23:56:33 +0530

    Time: 26 ms (66.68%), Space: 10.9 MB (36.83%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        if (k >= s.size()) return s;
-
-        string res = "";
-        int counter = 1;
-        
-        for (int i = 0; i < s.size(); ++i)
-            if (res.empty()) 
-                res.push_back(s[i]);
-            else if (counter < k) {
-                if (s[i] == res.back()) ++counter;
-                else counter = 1;
-                
-                res.push_back(s[i]);
-
-                if (counter == k) {
-                    for (int j = 0; j < k; ++j)
-                        res.pop_back();
-
-                    counter = 1;
-                    int size = res.size();
-                    while (counter < size && res[size-counter-1] == res[size-counter])
-                        ++counter;
-                }
+         vector<pair<char,int>> st;
+        for(char c : s) {
+            if(st.empty() || st.back().first != c) {
+                st.push_back({c,1});
             }
-
-        return res;
+            else if(++st.back().second == k ) 
+                st.pop_back();
+        }
+        string result="";
+        for(auto t : st) {
+            result.append(t.second,t.first);
+        }
+        return result;
     }
 };
