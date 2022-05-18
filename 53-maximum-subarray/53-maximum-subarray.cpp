105f984fa95e67e8fe75b964cb63d802860afb79
Apurva Sharma <apurva.sharma866@gmail.com>
2022-05-18 09:21:45 +0530

    Done

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -3,8 +3,10 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         int dp[n];
+
         dp[0] = nums[0];
         for(int i=1; i<n; i++){
+
             dp[i] = max(0, dp[i-1]) + nums[i];
         }
         return *max_element(dp, dp + n);
