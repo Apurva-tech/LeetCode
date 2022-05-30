27931b0f815e35fb4cba3a13a1b661df1646c144
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2022-05-30 10:11:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (76.09%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(divisor == 1) return dividend;
-        long double sign = (dividend < 0 ^ divisor < 0) ? -1 : 1;    
-        long double ldividend = labs(dividend);
-        long double ldivisor = labs(divisor);
-     
-        long long res = exp(log(ldividend)-log(ldivisor)); 
+        // Find sign of resultant
+        long double sign = (dividend < 0 ^ divisor < 0) ? -1 : 1; 
         
-        if(res > INT_MAX)
-            return sign == 1 ? INT_MAX : INT_MIN;
+        if(divisor == 1) return  dividend; 
+        
+        // Take abs of long double
+        long double longDividend = labs(dividend);
+        long double longDivisor = labs(divisor);
+     
+        // res = exp (log(a) - log(b)) 
+        long long res = exp(log(longDividend)-log(longDivisor)); 
+        
+        if(res > INT_MAX) return sign == 1 ? INT_MAX : INT_MIN;
         
         return sign * (int)res;
     }
