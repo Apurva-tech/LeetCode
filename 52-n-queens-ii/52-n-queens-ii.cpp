a83a49c53254b19e30ac078b68298fa24581b037
Apurva Sharma <59837325+Apurva-tech@users.noreply.github.com>
2022-06-06 03:14:20 +0530

    Time: 22 ms (22.86%), Space: 9.6 MB (19.50%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void fun(vector<vector<string>>& ans, vector<string>& temp, int r, set<int>& c, set<int>& d1, set<int>& d2, int n) {
+        
+        if(r == n) {
+           ans.push_back(temp); 
+        }
+        
+        for(int i=0;i<n;i++) {
+            if(c.find(i) == c.end() && d1.find(r-i) == d1.end() && d2.find(r+i) == d2.end()) {
+                
+                c.insert(i); d1.insert(r-i); d2.insert(r+i);
+                
+                temp[r][i] = 'Q';
+                fun(ans, temp, r+1, c, d1, d2, n);
+                temp[r][i] = '.';
+                
+                c.erase(i); d1.erase(r-i); d2.erase(r+i);
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+        set<int> c, d1, d2;
+        string dots = "";
+        for(int i=0;i<n;i++) dots.push_back('.');
+        
+        vector<string> temp(n, dots);
+        vector<vector<string>> ans;
+        fun(ans, temp, 0, c, d1, d2, n);
+        
+        return ans.size();
+    }
+};
